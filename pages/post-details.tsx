0ab6216225af1dde472c33317f9bc837648c59dd
Williams Samuel <samwill300@gmail.com>
2022-07-21 16:19:23 +0100

    finish article reaction sidebar;

--- pages/post-details.tsx
+++ pages/post-details.tsx
@@ -4,7 +4,7 @@ const PostDetails = () => {
   return (
     <>
       <section className="mt-20">
-        <div className="container grid grid-cols-5 gap-20">
+        <div className="container grid grid-cols-7 gap-20">
           <Article />
           <Sidebar />
         </div>
